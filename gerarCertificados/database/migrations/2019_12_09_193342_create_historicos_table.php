9f90b4fb227226cd46dff2291bd860ba34326e1a
Vitor Carrilho <vitor.carrilho@hotmail.com>
2019-12-09 16:49:34 -0300

    Criação da model e migration do historico

---
+++ gerarCertificados/database/migrations/2019_12_09_193342_create_historicos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHistoricosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('historicos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('email');
+            $table->string('cpf');
+            $table->date('nascimento');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('historicos');
+    }
+}
