7114b2e0d54bda6328c1072e9a6f420bd0f4b883
Valdiney <valdiney.2@hotmail.com>
2019-12-11 13:30:16 -0200

    Layout em formulário de cadastro

--- gerarCertificados/resources/views/index/index.blade.php
+++ gerarCertificados/resources/views/index/index.blade.php
@@ -8,6 +8,12 @@
     <title>Gerador de Certificados</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' href={{ asset('css/bootstrap.css') }}>
+
+    <style>
+      body {
+        background:#e0e0e0;
+      }
+    </style>
     
 </head>
 <body>
@@ -27,29 +33,46 @@
     <div class="card-body">
         <form method="POST" action="{{ route('cadastrarCertificado')}}">
         @csrf
-          <div class="form-group">
-            <label for="nome">Nome do aluno:</label>
-            <input type="text" class="form-control" name="nome" id="nome">
+          
+        <div class="row">
+
+          <div class="col-md-4">
+            <div class="form-group">
+              <label for="nome">Nome do aluno:</label>
+              <input type="text" class="form-control" name="nome" id="nome" placeholder="Digite aqui...">
+            </div>
           </div>
           
-          <div class="form-group">
-            <label for="cpf">CPF:</label>
-            <input type="text" class="form-control" name="cpf" id="cpf">
-            <small id="emailHelp" class="form-text text-muted">Nunca compartilharemos seu CPF com ninguém.</small>
+          <div class="col-md-4">
+            <div class="form-group">
+              <label for="cpf">CPF do aluno:</label>
+              <input type="text" class="form-control" name="cpf" id="cpf" placeholder="Digite aqui...">
+              <small id="emailHelp" class="form-text text-muted">Nunca compartilharemos seu CPF com ninguém.</small>
+            </div>
+          </div>
+          
+          <div class="col-md-4">
+            <div class="form-group">
+            <label for="email">Email do aluno:</label>
+              <input type="text" class="form-control" name="email" id="email" placeholder="Digite aqui...">
+              <small id="emailHelp" class="form-text text-muted">Nunca compartilharemos seu email com ninguém.</small>
+            </div>
+          </div>
+          
+          <div class="col-md-4">
+            <div class="form-group">
+            <label for="conclusao">Data de Conclusao:</label>
+              <input type="date" class="form-control" name="conclusao" id="conclusao" placeholder="Digite aqui...">
+            </div>
+          </div>
+          
+          <div class="col-md-12" style="text-align:right;margin-top:20px">
+            <div class="form-group">
+              <button type="submit" class="btn btn-primary">Salvar</button>
+            </div>
           </div>
 
-          <div class="form-group">
-          <label for="email">Email:</label>
-            <input type="text" class="form-control" name="email" id="email">
-            <small id="emailHelp" class="form-text text-muted">Nunca compartilharemos seu email com ninguém.</small>
-          </div>
-
-          <div class="form-group">
-          <label for="conclusao">Conclusao:</label>
-            <input type="date" class="form-control" name="conclusao" id="conclusao">
-          </div>
-
-          <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
         </form>
     </div>
   </div>
--- gerarCertificados/resources/views/pdf/index.blade.php
+++ gerarCertificados/resources/views/pdf/index.blade.php
@@ -6,20 +6,25 @@
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Gerador de Certificados</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' href={{ asset('css/bootstrap.css') }}>
     
+    <!--<link rel='stylesheet' type='text/css' href={{ asset('css/bootstrap.css') }}>-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/paper-css/0.3.0/paper.css" rel="stylesheet" />
     <style>
         body {
+            padding:0;
+            margin:0;
         	font-family:candara;
-        	background:#0e0e0e;
         }
     	.certificado {
-    		width:900px!important;
-    		background:white;
     		border:1px solid #ededee;
-    		margin-top:30px;
     		border-left:100px solid #e70012;
+            padding:10px;
+            background:white;
+            background-image:url('https://www.imagemhost.com.br/images/2019/12/11/coracao.png')!important;
+            background-position:top center;
+            background-repeat:no-repeat;
+            background-size:500px!important;
+            height:752px!important
     	}
     	.certificado h1 {
     		text-align:center;
@@ -35,33 +40,29 @@
     	}
     </style>
 </head>
-<body>
+<body class="A4">
 
-<div class="container">
-	<div class="row">
-		<div class="certificado">
+<div class="row">
+	<div class="certificado">
 
-			<h1>Certificado</h1>
-			<div class="col-md-12 texto">
-				<p>
-					Certificamos que, __________________ CPF nº ____________________ concluiu o curso de 
-					_________________, na _______________ CNPJ nº _____________________ com carga horária de 
-					_______, realizado na data __/__/____
-			</div>
+		<h1>Certificado</h1>
+		<div class="col-md-12 texto">
+			<p>
+				Certificamos que, __________________ CPF nº ____________________ concluiu o curso de 
+				_________________, na _______________ CNPJ nº _____________________ com carga horária de 
+				_______, realizado na data __/__/____
+		</div>
 
-			<div class="col-md-12 conclusao">
-				<p><b>Salvador</b>, __/__/____</p> <br>
-				___________________________ <br>
-				___________________________ <br>
-				________
-			</div>
-			
+		<div class="col-md-12 conclusao">
+			<p><b>Salvador</b>, __/__/____</p> <br>
+			___________________________ <br>
+			___________________________ <br>
+			________
 		</div>
 		
 	</div>
+	
 </div>
-
-
 
 </body>
 </html>
